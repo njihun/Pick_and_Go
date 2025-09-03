c4a7f0cd44f2a99c0a7598a5768bde9f25c00f8b
njihun <njihun@kakao.com>
2025-09-04 02:11:52 +0900

    a

--- index.js
+++ index.js
@@ -125,6 +125,8 @@ window.addEventListener("message", (event) => {
     // 보안상 origin 체크 필수
     if (event.origin !== window.location.origin) return;
     console.log(event);
+    console.log(event.data);
+    console.log(event.data.data);
     
     const { loginData } = event.data;
     console.log("카카오 토큰 받음:", loginData);
