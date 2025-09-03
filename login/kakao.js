849e1afeb9b1ff0b435b76f8ad69044b4168ecf6
njihun <njihun@kakao.com>
2025-09-03 22:28:30 +0900

    aaa

--- login/kakao.js
+++ login/kakao.js
@@ -5,6 +5,8 @@ const url = 'https://43.201.115.135:8443/kakao/code';
 fetch(`${url}?code=${code}`).then((res) => {
     return res.text();
 }).then((data)=>{
+    console.log(data);
+    
     window.opener.postMessage({ data }, window.location.origin);
     // window.close();
 }).catch((err)=>{
