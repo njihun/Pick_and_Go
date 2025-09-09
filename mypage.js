9a4e09a9c3663fb03fd1ecdeb65e17556f40cdb2
njihun <njihun@kakao.com>
2025-09-09 21:07:05 +0900

    aa

--- mypage.js
+++ mypage.js
@@ -10,5 +10,5 @@ if (!sessionStorage.getItem('name')) {
     const sex = sessionStorage.getItem('sex');
     user[1].innerText = sex=='male'?'남':sex=='female'?'여':'알 수 없음.';
     const age = sessionStorage.getItem('age').slice(0, 4);
-    user[2].innerText = new Date().getFullYear()+1-Number(age);
+    user[2].innerText = (new Date().getFullYear()+1-Number(age))+'세';
 }
