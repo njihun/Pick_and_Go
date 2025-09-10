16b08156c7ef02b0a66daa3cfb864448f471b948
njihun <njihun@kakao.com>
2025-09-11 01:42:35 +0900

    aa

--- index.js
+++ index.js
@@ -17,91 +17,6 @@ const regionElement = document.querySelector('#recommend > form > div:nth-of-typ
 const regionDropDown = document.getElementById('cities');
 const overlay = document.getElementById('overlay');
 const burgerBar = document.querySelector('.burger-bar');
-if (regionDropDown) {
-    regionDropDown.innerHTML = '';
-    region.forEach(e => {
-        const city = document.createElement('div');
-        city.classList.add('city');
-        const name = document.createElement('span');
-        name.classList.add('name');
-        name.innerText = e.name;
-        city.appendChild(name);
-        const children = document.createElement('div');
-        children.classList.add('child');
-        e.child.forEach((e2) => {
-            const city2 = document.createElement('div');
-            city2.classList.add('city');
-            city2.tabIndex = 0;
-            const name = document.createElement('span');
-            name.classList.add('name');
-            name.dataset.id = e2.id
-            name.innerText = e2.name;
-            city2.appendChild(name);
-            children.appendChild(city2);
-        });
-        city.appendChild(children);
-        regionDropDown.appendChild(city);
-    });
-}
-
-document.querySelectorAll('footer > *:last-child > div > *').forEach((e) => {
-    e.style.backgroundImage = `url(./imgs/${e.dataset.social}.png)`;
-});
-
-regionElement.addEventListener('click', (e) => {
-    if (document.getElementById('cities').contains(e.target)) return;
-    if (regionDropDown.style.display) {
-        document.querySelector('.dropdown-icon').classList.remove('open');
-        regionDropDown.style.display = '';
-    } else {
-        document.querySelector('.dropdown-icon').classList.add('open');
-        regionDropDown.style.display = 'block';
-    }
-});
-
-document.querySelectorAll('#cities > .city > .child > .city').forEach((e) => {
-    e.addEventListener('click', () => {
-        parent = e.parentElement.parentElement.querySelector('span');
-        regionElement.querySelector('span').innerText = (parent.innerText + ' ' + e.innerText);
-        
-        document.querySelector('.dropdown-icon').classList.remove('open');
-        regionDropDown.style.display = '';
-    });
-});
-
-regionElement.addEventListener('focusout', (e) => {
-    if (Array.from(document.querySelectorAll('#cities > .city > .child')).some((e2)=>{
-        return e2.contains(e.relatedTarget)
-    })) return;
-    document.querySelector('.dropdown-icon').classList.remove('open');
-    regionDropDown.style.display = '';
-});
-
-function close() {
-    document.querySelectorAll('#modal > *').forEach((e) => {
-        e.style.display = '';
-    });
-    overlay.style.display = '';
-    document.body.style.overflow = '';
-}
-
-overlay.addEventListener('click', () => {
-    if (document.getElementById('user-data').style.display) return;
-    close();
-});
-
-document.querySelectorAll('.close').forEach((e)=> {
-    e.addEventListener('click', () => {
-        close();
-    });
-});
-
-document.getElementById('log').onclick = () => {
-    document.body.style.overflow = 'hidden';
-    const login = document.getElementById('login');
-    overlay.style.display = 'block';
-    login.style.display = 'block';
-};
 
 burgerBar.addEventListener('click', () => {
     const menuItems = document.querySelector('.menu-bar > :first-child:not(.menu-item)');
@@ -205,6 +120,91 @@ window.addEventListener("message", async (event) => {
     overlay.style.display = 'block';
     userData.style.display = 'block';
 });
+
+regionDropDown.innerHTML = '';
+region.forEach(e => {
+    const city = document.createElement('div');
+    city.classList.add('city');
+    const name = document.createElement('span');
+    name.classList.add('name');
+    name.innerText = e.name;
+    city.appendChild(name);
+    const children = document.createElement('div');
+    children.classList.add('child');
+    e.child.forEach((e2) => {
+        const city2 = document.createElement('div');
+        city2.classList.add('city');
+        city2.tabIndex = 0;
+        const name = document.createElement('span');
+        name.classList.add('name');
+        name.dataset.id = e2.id
+        name.innerText = e2.name;
+        city2.appendChild(name);
+        children.appendChild(city2);
+    });
+    city.appendChild(children);
+    regionDropDown.appendChild(city);
+});
+
+document.querySelectorAll('footer > *:last-child > div > *').forEach((e) => {
+    e.style.backgroundImage = `url(./imgs/${e.dataset.social}.png)`;
+});
+
+regionElement.addEventListener('click', (e) => {
+    if (document.getElementById('cities').contains(e.target)) return;
+    if (regionDropDown.style.display) {
+        document.querySelector('.dropdown-icon').classList.remove('open');
+        regionDropDown.style.display = '';
+    } else {
+        document.querySelector('.dropdown-icon').classList.add('open');
+        regionDropDown.style.display = 'block';
+    }
+});
+
+document.querySelectorAll('#cities > .city > .child > .city').forEach((e) => {
+    e.addEventListener('click', () => {
+        parent = e.parentElement.parentElement.querySelector('span');
+        regionElement.querySelector('span').innerText = (parent.innerText + ' ' + e.innerText);
+        
+        document.querySelector('.dropdown-icon').classList.remove('open');
+        regionDropDown.style.display = '';
+    });
+});
+
+regionElement.addEventListener('focusout', (e) => {
+    if (Array.from(document.querySelectorAll('#cities > .city > .child')).some((e2)=>{
+        return e2.contains(e.relatedTarget)
+    })) return;
+    document.querySelector('.dropdown-icon').classList.remove('open');
+    regionDropDown.style.display = '';
+});
+
+function close() {
+    document.querySelectorAll('#modal > *').forEach((e) => {
+        e.style.display = '';
+    });
+    overlay.style.display = '';
+    document.body.style.overflow = '';
+}
+
+overlay.addEventListener('click', () => {
+    if (document.getElementById('user-data').style.display) return;
+    close();
+});
+
+document.querySelectorAll('.close').forEach((e)=> {
+    e.addEventListener('click', () => {
+        close();
+    });
+});
+
+document.getElementById('log').onclick = () => {
+    document.body.style.overflow = 'hidden';
+    const login = document.getElementById('login');
+    overlay.style.display = 'block';
+    login.style.display = 'block';
+};
+
 
 function numberTypeLength(e) {
     e.value = e.value.slice(0, e.dataset.max);
