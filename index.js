30c0b4fcc7e202b49c902cd83128767b533e183d
njihun <njihun@kakao.com>
2025-09-11 10:22:45 +0900

    aa

--- index.js
+++ index.js
@@ -237,57 +237,58 @@ async function editUserData() {
 }
 window.editUserData = editUserData;
 
-
-regionDropDown.innerHTML = '';
-region.forEach(e => {
-    const city = document.createElement('div');
-    city.classList.add('city');
-    const name = document.createElement('span');
-    name.classList.add('name');
-    name.innerText = e.name;
-    city.appendChild(name);
-    const children = document.createElement('div');
-    children.classList.add('child');
-    e.child.forEach((e2) => {
-        const city2 = document.createElement('div');
-        city2.classList.add('city');
-        city2.tabIndex = 0;
+if (regionDropDown) {
+    regionDropDown.innerHTML = '';
+    region.forEach(e => {
+        const city = document.createElement('div');
+        city.classList.add('city');
         const name = document.createElement('span');
         name.classList.add('name');
-        name.dataset.id = e2.id
-        name.innerText = e2.name;
-        city2.appendChild(name);
-        children.appendChild(city2);
+        name.innerText = e.name;
+        city.appendChild(name);
+        const children = document.createElement('div');
+        children.classList.add('child');
+        e.child.forEach((e2) => {
+            const city2 = document.createElement('div');
+            city2.classList.add('city');
+            city2.tabIndex = 0;
+            const name = document.createElement('span');
+            name.classList.add('name');
+            name.dataset.id = e2.id
+            name.innerText = e2.name;
+            city2.appendChild(name);
+            children.appendChild(city2);
+        });
+        city.appendChild(children);
+        regionDropDown.appendChild(city);
     });
-    city.appendChild(children);
-    regionDropDown.appendChild(city);
-});
-
-regionElement.addEventListener('click', (e) => {
-    if (document.getElementById('cities').contains(e.target)) return;
-    if (regionDropDown.style.display) {
+    
+    regionElement.addEventListener('click', (e) => {
+        if (document.getElementById('cities').contains(e.target)) return;
+        if (regionDropDown.style.display) {
+            document.querySelector('.dropdown-icon').classList.remove('open');
+            regionDropDown.style.display = '';
+        } else {
+            document.querySelector('.dropdown-icon').classList.add('open');
+            regionDropDown.style.display = 'block';
+        }
+    });
+    
+    document.querySelectorAll('#cities > .city > .child > .city').forEach((e) => {
+        e.addEventListener('click', () => {
+            parent = e.parentElement.parentElement.querySelector('span');
+            regionElement.querySelector('span').innerText = (parent.innerText + ' ' + e.innerText);
+            
+            document.querySelector('.dropdown-icon').classList.remove('open');
+            regionDropDown.style.display = '';
+        });
+    });
+    
+    regionElement.addEventListener('focusout', (e) => {
+        if (Array.from(document.querySelectorAll('#cities > .city > .child')).some((e2)=>{
+            return e2.contains(e.relatedTarget)
+        })) return;
         document.querySelector('.dropdown-icon').classList.remove('open');
         regionDropDown.style.display = '';
-    } else {
-        document.querySelector('.dropdown-icon').classList.add('open');
-        regionDropDown.style.display = 'block';
-    }
-});
-
-document.querySelectorAll('#cities > .city > .child > .city').forEach((e) => {
-    e.addEventListener('click', () => {
-        parent = e.parentElement.parentElement.querySelector('span');
-        regionElement.querySelector('span').innerText = (parent.innerText + ' ' + e.innerText);
-        
-        document.querySelector('.dropdown-icon').classList.remove('open');
-        regionDropDown.style.display = '';
     });
-});
-
-regionElement.addEventListener('focusout', (e) => {
-    if (Array.from(document.querySelectorAll('#cities > .city > .child')).some((e2)=>{
-        return e2.contains(e.relatedTarget)
-    })) return;
-    document.querySelector('.dropdown-icon').classList.remove('open');
-    regionDropDown.style.display = '';
-});
+}
