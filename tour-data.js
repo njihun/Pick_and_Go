fafd1384e4f6c6ebc071feb05c04e75dcbfc62d0
njihun <njihun@kakao.com>
2025-09-15 15:21:17 +0900

    aa

--- tour-data.js
+++ tour-data.js
@@ -81,6 +81,6 @@ async function star() {
 window.star = star;
 
 // 처음 로드될 때 관심 관광지인지 확인할 것
-if ((await getInterTour()).indexOf(tourId)!=-1) {
+if ((await getInterTour().map((e) => e["tour_id"])).indexOf(tourId)!=-1) {
     document.getElementById('star').classList.add('open');
 }
