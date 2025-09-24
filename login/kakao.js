895c72dbb5804e26b3e2b18b28ce841115a2a5b5
njihun <njihun@kakao.com>
2025-09-24 16:31:45 +0900

    로그인 오류 테스트

--- login/kakao.js
+++ login/kakao.js
@@ -2,13 +2,19 @@ const params = new URLSearchParams(location.search);
 const code = params.get('code');
 
 const url = 'https://d0g0h1.world/kakao/code';
+
 fetch(`${url}?code=${code}`).then((res) => {
     return res.text();
 }).then((data)=>{
-    window.opener.postMessage({ data }, window.location.origin);
-    setTimeout(() => {
-        window.close();
-    }, 100);
+    if (window.opener) {
+        document.body.innerHTML = window.opener;
+        window.opener.postMessage({ data }, window.location.origin);
+        setTimeout(() => {
+            window.close();
+        }, 1000);
+    } else {
+        document.body.innerHTML = "window.opener 없음."
+    }
 }).catch((err)=>{
     document.body.innerHTML = 'Failed to Redirect.';
     window.opener.postMessage({ data }, window.location.origin);
