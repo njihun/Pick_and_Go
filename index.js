24527e729fe084f6b6f94563d94b82b45f0fc06b
njihun <njihun@kakao.com>
2025-10-27 03:52:57 +0900

    login

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ document.querySelector('.social > div > div:nth-of-type(1)').addEventListener('c
 
 function login(user) {
     console.log('로그인 성공: '+user);
+    sessionStorage.setItem('temp', user);
     sessionStorage.setItem('jwt', data.PIGO_token);
     sessionStorage.setItem('name', user.name);
     sessionStorage.setItem('email', user.email);
