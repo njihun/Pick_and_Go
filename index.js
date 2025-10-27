f10baed5ada500c25e4fd07c5da80211670c2022
njihun <njihun@kakao.com>
2025-10-27 23:01:30 +0900

    reflect changes in user data

--- index.js
+++ index.js
@@ -247,6 +247,14 @@ async function editUserData() {
         sessionStorage.setItem('sex', res.sex);
         sessionStorage.setItem('age', res.age);
         sessionStorage.setItem('id', res.ID);
+        
+        document.querySelector('#profile > p > span').innerText = sessionStorage.getItem('name');
+        let user = document.querySelectorAll('#user > div:nth-of-type(2) > div');
+        user[0].innerText = sessionStorage.getItem('email');
+        const sex = sessionStorage.getItem('sex');
+        user[1].innerText = sex=='male'?'남':sex=='female'?'여':'알 수 없음.';
+        const age = sessionStorage.getItem('age').slice(0, 4);
+        user[2].innerText = (new Date().getFullYear()+1-Number(age))+'세';
         close();
         editing = false;
     }
